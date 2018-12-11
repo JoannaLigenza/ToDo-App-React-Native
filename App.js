e3ba1e2474086a20464923f547614e500b6ef5ab
JoannaLigenza <ligenzajoanna@gmail.com>
2018-12-11 11:57:23 +0100

    transition between screens added

--- App.js
+++ App.js
@@ -15,10 +15,24 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     //justifyContent: 'space-between',
   },
+  MenuButton: {
+    marginLeft: 15,
+  }
 });
 
 class HomeScreen extends Component {
-  static navigationOptions = { title: 'Home'};
+  static navigationOptions = ({ navigation }) => {
+    return {
+    title: 'Home', 
+    headerLeft: (
+        <Button
+          onPress={() => navigation.navigate('TaskEdit')}
+          title="X"
+          color="white"
+          style={styles.MenuButton}
+        />),
+    }
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -40,6 +54,7 @@ class TaskEdit extends Component {
       <View style={styles.container}>
         <EditTask />
         <Button title="Home" onPress={() => {this.props.navigation.goBack()}}></Button>
+        <Button title="Home" onPress={() => {this.props.navigation.navigate('Tesst')}}></Button>
       </View>
     );
   }
@@ -75,6 +90,56 @@ const Navi = createStackNavigator(
         fontWeight: 'bold',
       },
     },
+    mode: 'card',
+    //headerMode: 'none',
+    
+    // navigationOptions: params => ({
+    //   gesturesEnabled: true,
+    //   gesturesDirection: 'inverted',
+    // }),
+    
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+      },
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
+        const width = layout.initWidth;
+        console.log("index ", index)
+
+        return {
+          opacity: position.interpolate({
+            inputRange: [ index, index + 1],
+            outputRange: [1, 1],                    // [1, 0] opacity is from 1 to 0
+          }),
+          transform: [{
+            translateX: position.interpolate({
+              inputRange: [index - 1, index],       // index means, that transform is working for every screen (screnn index: 0, 1, 2 ... 50)
+              outputRange: [width, 0],            // [-width, 0] - slide is moving from left, [width, 0] - slide is moving from right
+              
+            }),
+          }]
+        };
+      },
+      // headerTitleInterpolator: sceneProps => {
+      //   const { position, scene } = sceneProps;
+      //   const { index } = scene;
+
+      //   return {
+      //     opacity: position.interpolate({
+      //       inputRange: [index - 1, index, index + 1],
+      //       outputRange: [ 0, 1, 0],
+      //     }),
+      //     transform: [{
+      //       translateX: position.interpolate({
+      //         inputRange: [index - 1, index, index + 1],
+      //         outputRange: [-width, 0, width]                // or [width, 0, 0]
+      //       }),
+      //     }]
+      //   };
+      // },
+    }),
   },
 );
 
