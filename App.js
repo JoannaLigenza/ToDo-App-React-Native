8a4d46aa89109a8721bff1b92711a16197ce64af
JoannaLigenza <ligenzajoanna@gmail.com>
2019-01-25 23:26:11 +0100

    unnecessary code commented

--- App.js
+++ App.js
@@ -64,7 +64,7 @@ export default class App extends Component {
   }
 
   setDeletedList = async (list) => {
-    console.log('deleted list', list)
+    //console.log('deleted list', list)
     await this.setState({ deletedList: list});
     this.setDataToAsyncStore();
   }
