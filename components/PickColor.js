edfeeed28178eab178d689da6d7084c0de4ce6b8
JoannaLigenza <ligenzajoanna@gmail.com>
2019-01-27 14:09:28 +0100

    ScrollView added for colors container

--- components/PickColor.js
+++ components/PickColor.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Image, TouchableOpacity, TouchableHighlight, Dimensions} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, Image, TouchableOpacity, TouchableHighlight, Dimensions} from 'react-native';
 import { background, darkGreyColor } from "./styles/commonStyles";
 
 export default class PickColor extends Component {
@@ -36,43 +36,40 @@ export default class PickColor extends Component {
                     Change color of Your App:
                 </Text>
             </View>
-            <View style={styles.colorContainer }>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#fec538', borderColor: (this.state.choosenColor === '#fec538') ? (darkGreyColor) : ('#fec538') }]}
-                    onPress={() => {this.setAppColor('#fec538'); }}>
-                    <Text style={styles.textColor} >Orange</Text>
-                </TouchableOpacity>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#75ff5a', borderColor: (this.state.choosenColor === '#75ff5a') ? (darkGreyColor) : ('#75ff5a') }]}
-                    onPress={() => {this.setAppColor('#75ff5a');  }}>
-                    <Text style={styles.textColor} >Light Green</Text>
-                </TouchableOpacity>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#0ccd43', borderColor: (this.state.choosenColor === '#0ccd43') ? (darkGreyColor) : ('#0ccd43') }]}
-                    onPress={() => {this.setAppColor('#0ccd43');  }}>
-                    <Text style={styles.textColor} >Dark Green</Text>
-                </TouchableOpacity>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#83b5ff', borderColor: (this.state.choosenColor === '#83b5ff') ? (darkGreyColor) : ('#83b5ff') }]}
-                    onPress={() => {this.setAppColor('#83b5ff');  }}>
-                    <Text style={styles.textColor} >Light Blue</Text>
-                </TouchableOpacity>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#2196f3', borderColor: (this.state.choosenColor === '#2196f3') ? (darkGreyColor) : ('#2196f3') }]}
-                    onPress={() => {this.setAppColor('#2196f3');  }}>
-                    <Text style={styles.textColor} >Dark Blue</Text>
-                </TouchableOpacity>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#d267f1', borderColor: (this.state.choosenColor === '#d267f1') ? (darkGreyColor) : ('#d267f1') }]}
-                    onPress={() => {this.setAppColor('#d267f1');  }}>
-                    <Text style={styles.textColor} >Lila</Text>
-                </TouchableOpacity>
-                <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: 'red', borderColor: (this.state.choosenColor === 'red') ? (darkGreyColor) : ('red') }]}
-                    onPress={() => {this.setAppColor('red');  }}>
-                    <Text style={styles.textColor} >Red</Text>
-                </TouchableOpacity>
-            </View>
+            <ScrollView >
+                <View style={styles.contentContainer }>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#fec538', borderColor: (this.state.choosenColor === '#fec538') ? (darkGreyColor) : ('#fec538') }]}
+                        onPress={() => {this.setAppColor('#fec538'); }}>
+                        <Text style={styles.textColor} >Orange</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#75ff5a', borderColor: (this.state.choosenColor === '#75ff5a') ? (darkGreyColor) : ('#75ff5a') }]}
+                        onPress={() => {this.setAppColor('#75ff5a');  }}>
+                        <Text style={styles.textColor} >Light Green</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#0ccd43', borderColor: (this.state.choosenColor === '#0ccd43') ? (darkGreyColor) : ('#0ccd43') }]}
+                        onPress={() => {this.setAppColor('#0ccd43');  }}>
+                        <Text style={styles.textColor} >Dark Green</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#83b5ff', borderColor: (this.state.choosenColor === '#83b5ff') ? (darkGreyColor) : ('#83b5ff') }]}
+                        onPress={() => {this.setAppColor('#83b5ff');  }}>
+                        <Text style={styles.textColor} >Light Blue</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#2196f3', borderColor: (this.state.choosenColor === '#2196f3') ? (darkGreyColor) : ('#2196f3') }]}
+                        onPress={() => {this.setAppColor('#2196f3');  }}>
+                        <Text style={styles.textColor} >Dark Blue</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: '#d267f1', borderColor: (this.state.choosenColor === '#d267f1') ? (darkGreyColor) : ('#d267f1') }]}
+                        onPress={() => {this.setAppColor('#d267f1');  }}>
+                        <Text style={styles.textColor} >Lila</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={[styles.color, {backgroundColor: 'red', borderColor: (this.state.choosenColor === 'red') ? (darkGreyColor) : ('red') }]}
+                        onPress={() => {this.setAppColor('red');  }}>
+                        <Text style={styles.textColor} >Red</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
             
-            {/* <Button
-                //onPress={() => this.props.navigation.goBack()}
-                onPress={() => {this.props.navigation.goBack(); this.props.screenProps.setPrimaryColor(this.state.choosenColor)}} 
-                title="Go back home"
-            /> */}
-            <TouchableOpacity activeOpacity={0.9} 
+            <TouchableOpacity activeOpacity={0.85} 
                 onPress={() => {this.props.navigation.goBack(); this.props.screenProps.setPrimaryColor(this.state.choosenColor)}} >
                 <Text style={[styles.button, styles.goBackButton]} >Save</Text>
             </TouchableOpacity>
@@ -94,8 +91,9 @@ const styles = StyleSheet.create({
         justifyContent:'center',
         margin: 7,
     },
-    colorContainer: {
+    contentContainer: {
         //flexDirection: 'row',
+        height: Dimensions.get('window').height - 100,
         backgroundColor: background,
         alignItems: 'center',
     },
